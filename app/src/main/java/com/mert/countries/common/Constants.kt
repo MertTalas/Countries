2cadd55ea466fc3e505387a9374f93f43e2452be
Mert <218CS2101@isik.edu.tr>
2022-08-04 01:55:53 +0300

    Resource and Constants implemented

---
+++ app/src/main/java/com/mert/countries/common/Constants.kt
@@ -0,0 +1,10 @@
+package com.mert.countries.common
+
+object Constants {
+
+    const val API_KEY = "a9e8f1f95bmshd38372b7d9c2bc3p130d8fjsnb0878cb484e3"
+    const val WIKI_DATA_URL = "https://www.wikidata.org/wiki/"
+    const val PREF_NAME = "country"
+    const val LIMIT = "10"
+    const val SHARED_PREFERENCES_KEY = "countries"
+}
